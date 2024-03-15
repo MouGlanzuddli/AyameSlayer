14ba3355a3d0cf1b24b0cfc1377d83386206d7ba
Mounnie ? <87159279+MouGlanzuddli@users.noreply.github.com>
2024-03-15 14:02:13 +0700

    Add files via upload

---
+++ Lane.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Melanchall.DryWetMidi.Interaction;
+
+public class Lane : MonoBehaviour
+{
+    public Melanchall.DryWetMidi.MusicTheory.NoteName noteRestriction;
+    public KeyCode input;
+    public GameObject notePrefab;
+    List<Note> notes = new List<Note>();
+    public List<double> timeStamps = new List<double>();
+
+    public float noteSpawnLeftX;
+    public float noteSpawnRightX;
+
+    int spawnIndex = 0;
+    int inputIndex = 0;
+
+    public void SetTimeStamps(Melanchall.DryWetMidi.Interaction.Note[] array)
+    {
+        foreach (var note in array)
+        {
+            if (note.NoteName == noteRestriction)
+            {
+                var metricTimeSpan = TimeConverter.ConvertTo<MetricTimeSpan>(note.Time, SongManager.midiFile.GetTempoMap());
+                timeStamps.Add((double)metricTimeSpan.Minutes * 60f + metricTimeSpan.Seconds + (double)metricTimeSpan.Milliseconds / 1000f);
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (spawnIndex < timeStamps.Count)
+        {
+            if (SongManager.GetAudioSourceTime() >= timeStamps[spawnIndex] - SongManager.Instance.noteTime)
+            {
+                var note = Instantiate(notePrefab, transform);
+                notes.Add(note.GetComponent<Note>());
+                note.GetComponent<Notes>().assignedTime = (float)timeStamps[spawnIndex];
+
+                float spawnX;
+                if (spawnIndex % 2 == 0)
+                {
+                    spawnX = SongManager.Instance.noteSpawnLeftX;
+                }
+                else
+                {
+                    spawnX = SongManager.Instance.noteSpawnRightX;
+                }
+
+                note.transform.position = new Vector3(spawnX, SongManager.Instance.noteSpawnY, transform.position.z);
+
+                spawnIndex++;
+            }
+        }
+
+        if (inputIndex < timeStamps.Count)
+        {
+            double timeStamp = timeStamps[inputIndex];
+            double marginOfError = SongManager.Instance.marginOfError;
+            double audioTime = SongManager.GetAudioSourceTime() - (SongManager.Instance.inputDelay / 1000.0);
+
+            if (Input.GetKeyDown(input))
+            {
+                if (Math.Abs(audioTime - timeStamp) < marginOfError)
+                {
+                    Hit();
+                    print($"Hit on {inputIndex} note");
+                    Destroy(gameObject);
+                    inputIndex++;
+                }
+                else
+                {
+                    print($"Hit inaccurate on {inputIndex} note with {Math.Abs(audioTime - timeStamp)} delay");
+                }
+            }
+            if (timeStamp + marginOfError <= audioTime)
+            {
+                Miss();
+                print($"Missed {inputIndex} note");
+                inputIndex++;
+            }
+        }
+
+    }
+    private void Hit()
+    {
+        ScoreManager.Hit();
+    }
+    private void Miss()
+    {
+        ScoreManager.Miss();
+    }
+}
